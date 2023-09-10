17358e0248249d9cbb8f559b217f51a674789a8d
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-10 11:47:44 -0700

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
+/**
+ *a program to print text using puts
+ */
 
 int main(void)
 {
 	const char *message = "\"Programming is like building a multilingual puzzle";
-	
+
 	puts(message);
-    	return (0);
+	return (0);
 }
