f21797fc946b418677eae6fa8acacd56c7106405
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-10 16:09:52 -0700

    working with modulo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,11 +27,11 @@ int main(void)
 	else
 	{
 		if ((remainder < 6) && (remainder != 0))
-			printf("Last digit of %d is %d and is
-			less than 6 and not 0\n", n, remainder);
+			printf("Last digit of %d is %d and is "
+			"less than 6 and not 0\n", n, remainder);
 		else
-			printf("Last digit of %d is %d
-			and is greater than 5\n", n, remainder);
+			printf("Last digit of %d is %d "
+			"and is greater than 5\n", n, remainder);
 	}
 	return (0);
 }
