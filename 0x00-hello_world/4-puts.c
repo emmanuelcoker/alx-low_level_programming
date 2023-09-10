a258a11b634d0c0917c6399d245772f6cfc5d833
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-10 11:05:20 -0700

    print message using putchar

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@ int main (void)
 	const char *message = "\"Programming is like building a multilingual puzzle\n";
 	int i = 0;
 
-	while (message[i] != "\o") {
+	while (message[i] != '\o') {
         	putchar(message[i]);
         	i++;
     	}
