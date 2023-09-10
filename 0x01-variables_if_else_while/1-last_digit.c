cc69b5192aa4abb5ea56e8112f52ddbc5bdc8447
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-10 15:53:00 -0700

    working with  modulo

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: program to determing list digit of numbers
+ * Return: Always return 0 for success
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n % 10 == 0)
+	{
+		printf("is %d and is 0\n", n);
+	}
+	else
+	{
+		if ((n % 10 < 6) && (n % 10 != 0))
+			printf("is %d and is less than 6 and not 0\n", n);
+		else
+			printf("is %d and is greater than 5\n", n);
+	}
+	return (0);
+}
