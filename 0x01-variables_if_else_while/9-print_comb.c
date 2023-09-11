0269debee773a91a9cb7fe35b5f4e15fb3204a2f
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-11 14:31:16 -0700

    print digits with comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry into the program
+ *
+ * Description: a program to print combination of single
+ * digit numbers separated with comma
+ * Return: Always return 0 when successful
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		if (i < 57)
+			putchar(44);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
