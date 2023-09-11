ea1be629c0978d6685e956f7ab8499663dc5604f
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-11 14:20:16 -0700

    print digits of hexadecimal system using putchar

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry into the program
+ *
+ * Description: A program to print hexadecimal digits using putchar
+ * Return: Always  return 0 when successful
+ */
+
+int main(void)
+{
+	char hexa_digits[] = "0123456789ABCDE";
+	int i = 0;
+
+	while (i < 16)
+	{
+		putchar(hexa_digits[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
