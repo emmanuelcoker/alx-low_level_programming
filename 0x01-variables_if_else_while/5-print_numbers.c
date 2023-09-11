49f53cd8287136e93ad29d04f393da63eafa8019
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-11 12:19:36 -0700

    print numbers from 0 to 9 using putchar

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point to the program
+ *
+ * Description: a program to print all digits in decimal
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
