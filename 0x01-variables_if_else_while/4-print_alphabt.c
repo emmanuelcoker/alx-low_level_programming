ece236cd7b697a8f14b35fe85fbf2b399a6661ea
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-11 12:09:06 -0700

    print all lowercase alphabets except q and e using putchar

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- Entry into the program
+ *
+ * Description: a program that prints lowercase except q and e
+ * Return: Always 0 when successful
+ */
+
+int main(void)
+{
+        char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+        int i = 0;
+
+        while (i < 26)
+        {
+		if (alphabets[i] !== 'q' || alphabets[i] !== 'e')
+			putchar(alphabets[i]);
+                i++;
+        }
+        putchar('\n');
+        return (0);
+}
