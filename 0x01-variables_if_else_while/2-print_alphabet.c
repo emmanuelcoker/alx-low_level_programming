c0d77eb101365748e41ea7daec1ee1ac3bc66fda
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-11 11:49:10 -0700

    print alphabet letters in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-/*
- *main - Entry to the program
- *
- *description: a program that transforms a letter into lowercase
- *return: Always 0 for success
- */
+/**
+  *main - Entry to the program
+  *
+  *description: a program that transforms a letter into lowercase
+  *return: Always 0 for success
+  */
 
 int main(void)
 {
-	char alphabets[] = "abcdefghijklmnopqrstuvwxyz\n";
+	char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
 	int i = 0;
 
 	while (i < 26)
@@ -16,5 +16,6 @@ int main(void)
 		putchar(alphabets[i]);
 		i++;
 	}
+	putchar('\n');
 	return (0);
 }
