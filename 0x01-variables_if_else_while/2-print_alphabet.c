2f93f52fa38c1aef4426e4a470210a088a28490b
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-11 11:36:09 -0700

    init print lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/*
+ *main - Entry to the program
+ *
+ *Description: a program that transforms a letter into lowercase
+ *Return: Always 0 for success
+ */
+
+int main(void)
+{
+	return (0);
+}
