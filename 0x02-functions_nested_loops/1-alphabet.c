59e4214a669fea0f79fbb35d0d6b1822ec867f3b
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-12 13:27:27 -0700

    print lowercase alphabets using function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Enntry into the program
+ *
+ * Description: a program that prints
+ * Return : always return 0 when successful
+ */
+
+int main(void)
+{
+	print_alphabets();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+void print_alphatet(void);
+
+void print_alphabet(void)
+{
+	char *alphabets = "abcdefghijklmnopqrstuvwxyz";
+
+	printf("%s\n", alphabets);
+}
