4daa4d55f82d247d0d6cfda2fc8c0e08b199543c
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-12 15:23:40 -0700

    check if a character is alphabet

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - show the sign of an integer value
+ *
+ * @n: n is an integer value
+ * Description: print + if greater thn 0 and - if lless and 0 if zero
+ * Return: return 1 if greater, -1 if less than and 0 if zero
+ */
+
+int print_sign(int n)
+{
+	char plus = '+';
+	char minus = '-';
+
+	if(n > 0)
+	{
+		_putchar(plus);
+		retutn (1);
+	}
+	else if(n < 0)
+	{
+		_putchar(minus);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+
+
+}
