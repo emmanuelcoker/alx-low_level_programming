33c46ba70a19ec262827a41f167c35cf01d30673
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-12 13:07:19 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry into the program
+ *
+ * Description: A program to print _putchar
+ * Return: return 0 when successful
+ */
+
+int main(void)
+{
+	char *message = "_putchar";
+
+	printf("%s\n", message);
+	return (0);
+}
