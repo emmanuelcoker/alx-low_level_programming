0060c33c958f0ba7c62b040a42fe0a0c65792fc3
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-12 15:41:35 -0700

    get absollute value of an int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - convert number to absolute value
+ *
+ * @n: integer number
+ * Description: convert signed integer to absolute val
+ * Return: return 0 when successful
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+		printf("%d\n", n);
+	else
+		printf("%d\n", (n * -1));
+	return (0);
+}
