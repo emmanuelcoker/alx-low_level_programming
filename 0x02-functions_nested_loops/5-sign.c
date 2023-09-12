113f461d52be513c868b331ff8e59f3b9d99e8d1
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-12 15:28:23 -0700

    check if a character is alphabet

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,12 +14,12 @@ int print_sign(int n)
 	char plus = '+';
 	char minus = '-';
 
-	if(n > 0)
+	if (n > 0)
 	{
 		_putchar(plus);
 		return (1);
 	}
-	else if(n < 0)
+	else if (n < 0)
 	{
 		_putchar(minus);
 		return (-1);
@@ -29,6 +29,4 @@ int print_sign(int n)
 		_putchar(48);
 		return (0);
 	}
-
-
 }
