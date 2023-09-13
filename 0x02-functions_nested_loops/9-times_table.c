319cc6e23026d38d58fe61aa102e30a25fde0460
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-13 16:39:50 -0700

    print to table 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - print times table
+ *
+ * Description: print table 9
+ * Return: return void
+ */
+
+void times_table(void)
+{
+	int row = 0;
+	int col = 0;
+	int multiple;
+	int limit = 9;
+
+	for (row = 0; row <= limit; row++)
+	{
+		for (col = 0; col <= limit; col++)
+		{
+			multiple = row * col;
+			_putchar(multiple + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
