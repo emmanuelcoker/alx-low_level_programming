65f553359be6947bb02440d4d7f7bcbd74e273fe
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-13 13:38:02 -0700

    jackbauer, print miutes of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * jack_bauer - display minutes of the day
+ *
+ * description: print minutes of the day with new line
+ * Return: return 0 when successful
+ */
+
+void jack_bauer(void)
+{
+}
