8aa008051c46410cd969bf3bd934a911ba96b1b9
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-14 14:42:00 -0700

    check if value is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - checks if value is a digig
+ *
+ * @c: ascii code for the digit
+ * Description: a program to check if adigit is between 0 to 9
+ * Return: return 1 if digit else return 0
+ */
+
+int _isdigit(int c)
+{
+	if(c >= '0' && c <= '9')
+		return (1)
+	return (0);
+}
