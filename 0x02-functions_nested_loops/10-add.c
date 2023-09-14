d0f9b581dac6089fea69b3119daa99756844f9d9
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-13 17:22:44 -0700

    add two digits

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add -  a function that add two numbers
+ *
+ * Description: add tow integers
+ * Return: returns the sum of the integers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
