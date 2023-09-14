fe6b8fea7ae3bdeb185320818f1525d40d34890d
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-14 13:27:57 -0700

    test the result of positive_or_negative function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry into the program
+ *
+ * Description: test the function positive_or_negative
+ * Return: return 0 when successful
+ */
+
+int main(void )
+{
+	int zero = 0;
+	
+	positive_or_negative(zero);
+	return (0);
+}
