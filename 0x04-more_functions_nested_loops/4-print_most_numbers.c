dc2037fd86d6923dca89760bb2cbcfc167076569
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-14 15:47:45 -0700

    print 0 to 9 except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most__numbers - print 0 to 9 except 2 and 4
+ *
+ * Description: print numbers from 0 to 9 except 2 and 4 then a new line
+ * Return: no return
+ */
+
+void print_most_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		if ((i != 50) && (i != 52))
+			_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
