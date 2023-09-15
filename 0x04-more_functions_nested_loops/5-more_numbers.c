613c059c9a87ebed1300804dcdd0597eebf4bff0
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-15 15:14:04 -0700

    print 0 to 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers 0 to 14 ten times
+ *
+ * Description: print 0 to 14 ten times using _putchar
+ * Return: return void
+ */
+
+void more_numbers(void)
+{
+	int i = 0;
+	int ch = 0;
+
+	while (i < 10)
+	{
+		while (ch <= 14)
+		{
+			if (ch >= 10)
+			{
+				_putchar((ch / 10) + '0');
+				_putchar((ch % 10) + '0');
+			}
+			else
+			{
+				_putchar(ch + '0');
+			}
+		}
+		ch = 0;
+		_putchar('\n');
+	}
+}
