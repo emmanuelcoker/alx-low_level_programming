c1f8fe91ccc0537310c43fc52e4e4499a966bd2d
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-15 14:32:47 -0700

    added main

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+
+
+#endif
