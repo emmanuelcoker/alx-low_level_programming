aec5729d8622f869518ce3452a112248c5a5282e
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-18 16:32:07 -0700

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: string to be reversed
+ * Return: return void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+     	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	while (i != 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+
+	_putchar('\n');
+}
