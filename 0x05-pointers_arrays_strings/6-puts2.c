d268522108279cf4c7257c15854f5f4eacc8b9e1
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-19 16:11:28 -0700

    print some characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - print some characters only
+ * @str: string value to print
+ * Return: return void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(*(str + i));
+		i += 2;
+	}
+}
