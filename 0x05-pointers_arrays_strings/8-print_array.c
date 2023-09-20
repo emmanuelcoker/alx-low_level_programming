332806f89cf37197786e7a71fff82bb1bf57c540
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-19 17:39:12 -0700

    print values of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - print values of an array
+ * @a: array to be printed
+ * @n: lenth of the array
+ * Return: return void
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", *(a + i));
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	_putchar('\n');
+}
