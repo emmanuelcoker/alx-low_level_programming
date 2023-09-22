8a71d9ab567ad3baec326e8c54d91ac1280351d1
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-21 19:05:21 -0700

    convert string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *string_toupper - convert characer to uuppercase
+ * @str: characters to be cinverted
+ * Return: return char
+ */
+
+char *string_toupper(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str >= 'a' && *str <= 'z')
+		{
+			*str = *str - 32;
+		}
+		str++;
+	}
+}
+
