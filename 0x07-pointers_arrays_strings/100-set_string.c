cd2b4c7e1c9ebd780f8ab7e7adffbd8b39473865
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-09-26 14:22:33 -0700

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - set string to another string
+ * @s: double pointer to initial string
+ * @to: string to replace s
+ * Return: returns void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
