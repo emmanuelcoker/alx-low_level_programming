3d09942731a900b6125b5454ba135c16b28373da
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-10-03 14:17:32 -0700

    print name of progra

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print name of the executable
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: return 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+adding arguments to main
