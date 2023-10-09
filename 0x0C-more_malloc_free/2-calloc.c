b77d3ebcdf6744bae3ea8f6576f57d895ccda500
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-10-08 17:07:12 -0700

    fixed custom calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,27 +2,6 @@
 #include <stdlib.h>
 
 /**
- * *_memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
- *
- * Return: pointer to the memory area s
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-
-	return (s);
-}
-
-
-/**
  * *_calloc - allocate memory to array
  * @nmemb: number of member elements in array
  * @size: sizeof() value of the type of the array
@@ -39,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	ptr = malloc(nmemb * size);
 
-	for (i = 0; i < (nmemb * size); i++)
+	for (i = 0; i <= (nmemb * size); i++)
 		ptr[i] = 0;
 
 	if (ptr == NULL)
