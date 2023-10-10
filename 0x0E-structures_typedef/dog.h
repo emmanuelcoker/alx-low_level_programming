f93bc689e8ddd2d931c146f8b154d1ab6024e2a9
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-10-10 13:13:08 -0700

    created a struct for dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - defining dog struct with properties
+ * @name: name of dog as char
+ * @age:  age of dog as float
+ * @owner: owner of dog as char
+*/
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+};
+
+#endif
