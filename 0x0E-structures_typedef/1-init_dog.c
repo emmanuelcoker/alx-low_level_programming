cf3f36052849f07e1daad2a678037fc96efebcd9
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-10-10 13:22:17 -0700

    initiallize dog struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize dog properties
+ * @d: dog struct pointer
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner of dog
+ * Return: return void
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
