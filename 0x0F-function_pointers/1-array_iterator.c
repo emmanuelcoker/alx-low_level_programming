d236fbaab026343c77fb4308fc730edd9303c143
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-10-11 14:32:39 -0700

    iterating function pointer on array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - run a function on array elements
+ * @array: array of items
+ * @size: size of the array
+ * @action: function pointer
+ * Return: return nothing
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
