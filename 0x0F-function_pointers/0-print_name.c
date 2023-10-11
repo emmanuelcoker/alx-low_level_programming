630fb9a6dfa01e1f36cfa775b53826888c467ea5
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-10-11 14:24:20 -0700

    print name using function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+
+#include "function_pointers.h"
+
+/**
+ * print_name - print name as is or in uppercase
+ * @name: name to be printed
+ * @f: function pointer
+ * Return: return nothing
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
